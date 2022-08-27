85a576bcacd3789b97e91d0507bc12091de4ec5e
ImKyleJK <me@imkylejk.me>
2022-08-27 14:22:32 +0100

    Added description

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def read_root():
     return {"Hello": "World"}
 
 
+# localhost:8000/param-test/10?q=lol
+# Returns {"Para Value": 10, "q": "lol"}
 @app.get("/param-test/{para}")
 def read_item(para: int, q: Union[str, None] = None):
-    return {"Your Paramater was:": para, "q": q}
+    return {"Para Value": para, "q": q}
